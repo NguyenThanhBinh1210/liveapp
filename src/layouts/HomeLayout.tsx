f1b6c47f260cc9cba7b39edb87137d54aa8a6048
NguyenThanhBinh1210 <thanhbinh12369yt@gmail.com>
2025-06-19 20:29:40 +0700

    binh

--- src/layouts/HomeLayout.tsx
+++ src/layouts/HomeLayout.tsx
@@ -1,16 +1,17 @@
+import { useLocation } from 'react-router-dom'
 import Footer from '~/components/layouts/Footer'
 import Header from '~/components/layouts/Header'
 import { LayoutPropsInterface } from '~/types/base.type'
 
 const HomeLayout = ({ children }: LayoutPropsInterface) => {
-  return <div className='max-w-[600px] mx-auto w-full'>
-    <Header></Header>
-    <div className='dark:bg-[#212121] transition-all duration-300 min-h-screen'>
-      {children}
+  const path = useLocation().pathname
+  return (
+    <div className='max-w-[600px] mx-auto w-full'>
+      {path === '/message' ? <></> : <Header></Header>}
+      <div className='dark:bg-[#212121] transition-all duration-300 min-h-screen'>{children}</div>
+      <Footer />
     </div>
-    <Footer />
-  </div>
-
+  )
 }
 
 export default HomeLayout
--- src/pages/Message.tsx
+++ src/pages/Message.tsx
@@ -1,7 +1,419 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useEffect, useRef, useState } from 'react'
+import avatar from '~/assets/4sand.avif'
 
 const Message = () => {
   return (
-    <div>Message</div>
+    <div>
+      <div className='p-2 flex items-center gap-x-2  mb-2 justify-between sticky top-0 bg-white'>
+        <button className='p-2 rounded-full dark:text-white hover:bg-gray-200 dark:hover:bg-black/30 transition-all duration-300'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='size-6'
+          >
+            <path strokeLinecap='round' strokeLinejoin='round' d='M15.75 19.5 8.25 12l7.5-7.5' />
+          </svg>
+        </button>
+        <h2 className='text-lg font-bold dark:text-white absolute left-1/2 -translate-x-1/2'>Hộp thư</h2>
+        <button className='p-2 rounded-full dark:text-white hover:bg-gray-200 dark:hover:bg-black/30 transition-all duration-300'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='size-6'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z'
+            />
+          </svg>
+        </button>
+      </div>
+      <div className='p-2 space-y-3 pb-20'>
+        <Followers />
+
+        {Array.from({ length: 20 }).map((_, index) => (
+          <ChatBox key={index} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+const Followers = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = 'hidden'
+    } else {
+      document.body.style.overflow = 'auto'
+    }
+  }, [isOpen])
+  return (
+    <>
+      <div onClick={() => setIsOpen(true)} className='flex items-center justify-between cursor-pointer'>
+        <div className='flex items-center gap-2'>
+          <div className='bg-blue-400 rounded-full w-12 h-12 flex items-center justify-center text-white flex-shrink-0'>
+            <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor' className='size-6'>
+              <path d='M4.5 6.375a4.125 4.125 0 1 1 8.25 0 4.125 4.125 0 0 1-8.25 0ZM14.25 8.625a3.375 3.375 0 1 1 6.75 0 3.375 3.375 0 0 1-6.75 0ZM1.5 19.125a7.125 7.125 0 0 1 14.25 0v.003l-.001.119a.75.75 0 0 1-.363.63 13.067 13.067 0 0 1-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 0 1-.364-.63l-.001-.122ZM17.25 19.128l-.001.144a2.25 2.25 0 0 1-.233.96 10.088 10.088 0 0 0 5.06-1.01.75.75 0 0 0 .42-.643 4.875 4.875 0 0 0-6.957-4.611 8.586 8.586 0 0 1 1.71 5.157v.003Z' />
+            </svg>
+          </div>
+          <div className=''>
+            <p className='text-sm font-medium'>Những Follower mới</p>
+            <p className='text-xs text-gray-700'>
+              <strong>Mochi</strong> đã yêu cầu follow bạn.
+            </p>
+          </div>
+        </div>
+        <button className='p-2'>
+          <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor' className='size-4'>
+            <path
+              fillRule='evenodd'
+              d='M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z'
+              clipRule='evenodd'
+            />
+          </svg>
+        </button>
+      </div>
+      <div
+        onClick={(e) => {
+          setIsOpen(false)
+          e.stopPropagation()
+        }}
+        className={`fixed top-0 left-0 w-full h-full bg-black/50 z-[51] !mt-0 transition-all duration-300 ${isOpen ? 'opacity-100 visible' : 'opacity-0 invisible'
+          }`}
+      >
+        <div
+          onClick={(e) => e.stopPropagation()}
+          className={`max-w-[600px] dark:bg-[#212121] h-screen mx-auto bg-white transition-all duration-300 ${isOpen ? 'translate-x-0' : 'translate-x-full'
+            }`}
+        >
+          <div className='p-2 flex items-center gap-x-2  mb-2  bg-white'>
+            <button
+              onClick={() => setIsOpen(false)}
+              className='p-2 rounded-full dark:text-white hover:bg-gray-200 dark:hover:bg-black/30 transition-all duration-300'
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
+                className='size-6'
+              >
+                <path strokeLinecap='round' strokeLinejoin='round' d='M15.75 19.5 8.25 12l7.5-7.5' />
+              </svg>
+            </button>
+            <h2 className='text-lg font-bold dark:text-white absolute left-1/2 -translate-x-1/2'>
+              {' '}
+              Những Follower mới
+            </h2>
+          </div>
+          <div className='p-2 px-4 space-y-3  overflow-y-auto h-[calc(100vh-60px)] pb-4'>
+            {Array.from({ length: 20 }).map((_, index) => (
+              <div key={index} className='flex items-center justify-between cursor-pointer'>
+                <div className='flex items-center gap-2'>
+                  <img src={avatar} alt='' className='w-12 h-12 rounded-full flex-shrink-0' />
+                  <div className=''>
+                    <p className='text-sm font-medium'>Vô thượng sát thần</p>
+                    <p className='text-xs text-gray-700'>đã yêu cầu follow bạn</p>
+                  </div>
+                </div>
+                <button className='w-20  bg-[#f85671] rounded-full h-max text-xs font-medium py-1 text-white'>
+                  Chấp nhận
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+const ChatBox = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = 'hidden'
+    } else {
+      document.body.style.overflow = 'auto'
+    }
+  }, [isOpen])
+  const [inputValue, setInputValue] = useState('')
+
+  const initialMessages = [
+    { id: 1, type: 'text', text: 'Chào bạn!', sender: 'other' },
+    { id: 2, type: 'image', url: avatar, sender: 'me' },
+    { id: 3, type: 'text', text: 'Mèo cưng quá!', sender: 'other' },
+    { id: 4, type: 'video', url: 'https://www.w3schools.com/html/mov_bbb.mp4', sender: 'me' },
+    { id: 5, type: 'video', url: 'https://www.w3schools.com/html/mov_bbb.mp4', sender: 'other' },
+    { id: 6, type: 'audio', url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3', sender: 'me' },
+    { id: 7, type: 'text', text: 'Mèo cưng ghê', sender: 'me' },
+    { id: 8, type: 'text', text: 'Mèo cưng ghê', sender: 'me' },
+    { id: 9, type: 'text', text: 'Mèo cưng ghê', sender: 'me' },
+    { id: 10, type: 'text', text: 'Mèo cưng ghê', sender: 'me' },
+    { id: 11, type: 'text', text: 'Mèo cưng ghê nhưng mà mèo cưng lắm hahs', sender: 'me' }
+  ]
+
+  const [messages, setMessages] = useState<any>(initialMessages)
+  const handleSendMessage = () => {
+    const trimmed = inputValue.trim()
+    if (!trimmed) return
+
+    const newMessage = {
+      id: Date.now(), // hoặc uuid
+      type: 'text',
+      text: trimmed,
+      sender: 'me'
+    }
+
+    setMessages((prev: any) => [...prev, newMessage])
+    setInputValue('')
+
+    // optional: scroll to bottom
+    setTimeout(() => {
+      const container = document.getElementById('chat-container')
+      container?.scrollTo({ top: container.scrollHeight, behavior: 'smooth' })
+    }, 100)
+  }
+  const chatRef = useRef<HTMLDivElement>(null)
+  useEffect(() => {
+    if (chatRef.current) {
+      chatRef.current.scrollTo({
+        top: chatRef.current.scrollHeight,
+        behavior: 'smooth' // hoặc 'auto' nếu không muốn animation
+      })
+    }
+  }, [messages])
+  useEffect(() => {
+    if (chatRef.current) {
+      chatRef.current.scrollTo({
+        top: chatRef.current.scrollHeight,
+        behavior: 'auto'
+      })
+    }
+  }, [])
+
+  const [isKeyboardOpen, setIsKeyboardOpen] = useState(false)
+  const isMobileDevice = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (!isMobileDevice) return
+
+      const isKeyboard = window.innerHeight < window.screen.height - 100
+      setIsKeyboardOpen(isKeyboard)
+    }
+
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  return (
+    <>
+      <div onClick={() => setIsOpen(true)} className='flex items-center justify-between cursor-pointer'>
+        <div className='flex items-center gap-2'>
+          <img src={avatar} alt='' className='w-12 h-12 rounded-full flex-shrink-0' />
+          <div className=''>
+            <p className='text-sm font-medium'>Vô thượng sát thần</p>
+            <p className='text-xs text-gray-700'>giá đó hơn lb 300k thôi</p>
+          </div>
+        </div>
+        <button className='w-6  bg-[#f85699] rounded-full h-max text-[9px] text-white'>13</button>
+      </div>
+      <div
+        onClick={(e) => {
+          setIsOpen(false)
+          e.stopPropagation()
+        }}
+        className={`fixed top-0 left-0 w-full h-full bg-black/50 z-[51] !mt-0 transition-all duration-75 ${isOpen ? 'opacity-100 visible' : 'opacity-0 invisible'
+          }`}
+      >
+        <div
+          onClick={(e) => e.stopPropagation()}
+          className={`max-w-[600px] dark:bg-[#212121] h-screen mx-auto bg-[#f1f1f1] transition-all duration-75 ${isOpen ? 'translate-x-0' : 'translate-x-full'
+            }`}
+        >
+          <div className='p-2 flex items-center gap-x  m sticky top-0 border-b'>
+            <button
+              onClick={() => setIsOpen(false)}
+              className='p-2 rounded-full dark:text-white hover:bg-gray-200 dark:hover:bg-black/30 transition-all duration-300'
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
+                className='size-6'
+              >
+                <path strokeLinecap='round' strokeLinejoin='round' d='M15.75 19.5 8.25 12l7.5-7.5' />
+              </svg>
+            </button>
+            <div className='flex items-center justify-between cursor-pointer'>
+              <div className='flex items-center gap-2'>
+                <img src={avatar} alt='' className='size-10 rounded-full flex-shrink-0' />
+                <div className=''>
+                  <p className='text-sm font-medium'>Vô thượng sát thần</p>
+                  <p className='text-[10px] text-gray-700'>Hiện đang hoạt động</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            ref={chatRef}
+            className={`flex flex-col overflow-y-auto px-4 pt-2 pb-16 transition-all duration-300 space-y-3  ${isMobileDevice && isKeyboardOpen ? 'h-[calc(100vh-300px)]' : 'h-[calc(100vh-60px)]'
+              }`}
+          >
+            {messages.map((msg: any) => (
+              <div key={msg.id} className={`flex mt-auto ${msg.sender === 'me' ? 'justify-end' : 'justify-start'}`}>
+                <div
+                  className={`max-w-[75%] rounded-xl ${msg.type === 'text' ? 'p-2' : ''} text-sm ${msg.sender === 'me'
+                    ? 'bg-[#1590c0] text-white rounded-br-none'
+                    : 'bg-white text-black rounded-bl-none'
+                    }`}
+                >
+                  {/* Text Message */}
+                  {msg.type === 'text' && <p>{msg.text}</p>}
+
+                  {/* Image */}
+                  {msg.type === 'image' && (
+                    <img
+                      src={msg.url}
+                      alt='image'
+                      className={`w-40 h-40 object-cover rounded-lg ${msg.sender === 'me' ? 'rounded-br-none' : 'rounded-bl-none'
+                        }`}
+                    />
+                  )}
+
+                  {/* Video */}
+                  {msg.type === 'video' && (
+                    <video
+                      src={msg.url}
+                      controls
+                      className={`w-52 h-40 rounded-lg ${msg.sender === 'me' ? 'rounded-br-none' : 'rounded-bl-none'
+                        } object-cover`}
+                    />
+                  )}
+
+                  {/* Audio */}
+                  {msg.type === 'audio' && (
+                    <audio controls className='w-full mt-1'>
+                      <source src={msg.url} type='audio/mpeg' />
+                      Trình duyệt không hỗ trợ audio
+                    </audio>
+                  )}
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className='p-2 absolute bottom-0 left-0 right-0 bg-[#f1f1f1]'>
+            <div id='chat-container' className='flex items-center gap-1 bg-white rounded-full px-1 py-0.5'>
+              <button className='bg-blue-300 text-white size-8 rounded-full flex items-center justify-center flex-shrink-0'>
+                <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor' className='size-5'>
+                  <path d='M12 9a3.75 3.75 0 1 0 0 7.5A3.75 3.75 0 0 0 12 9Z' />
+                  <path
+                    fillRule='evenodd'
+                    d='M9.344 3.071a49.52 49.52 0 0 1 5.312 0c.967.052 1.83.585 2.332 1.39l.821 1.317c.24.383.645.643 1.11.71.386.054.77.113 1.152.177 1.432.239 2.429 1.493 2.429 2.909V18a3 3 0 0 1-3 3h-15a3 3 0 0 1-3-3V9.574c0-1.416.997-2.67 2.429-2.909.382-.064.766-.123 1.151-.178a1.56 1.56 0 0 0 1.11-.71l.822-1.315a2.942 2.942 0 0 1 2.332-1.39ZM6.75 12.75a5.25 5.25 0 1 1 10.5 0 5.25 5.25 0 0 1-10.5 0Zm12-1.5a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z'
+                    clipRule='evenodd'
+                  />
+                </svg>
+              </button>
+              <input
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                onKeyDown={(e) => e.key === 'Enter' && handleSendMessage()}
+                onFocus={() => setIsKeyboardOpen(true)}
+                onBlur={() => setIsKeyboardOpen(false)}
+                type='text'
+                placeholder='Nhập tin nhắn...'
+                className='w-full rounded-full p-2 text-sm'
+              />
+              <div className='flex gap-1 pr-2'>
+                <button className=' text-black size-8 rounded-full flex items-center justify-center flex-shrink-0'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    strokeWidth={1.5}
+                    stroke='currentColor'
+                    className='size-5'
+                  >
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      d='M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z'
+                    />
+                  </svg>
+                </button>
+                <button className=' text-black size-8 rounded-full flex items-center justify-center flex-shrink-0'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    strokeWidth={1.5}
+                    stroke='currentColor'
+                    className='size-6'
+                  >
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      d='M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z'
+                    />
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      d='M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z'
+                    />
+                  </svg>
+                </button>
+                <button
+                  className={` text-black w-8 h-8 rounded-full flex items-center transition-all duration-300 justify-center flex-shrink-0 ${inputValue ? 'w-0' : 'w-8'
+                    }`}
+                >
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    strokeWidth={1.5}
+                    stroke='currentColor'
+                    className='size-6'
+                  >
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      d='m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z'
+                    />
+                  </svg>
+                </button>
+                <button
+                  onClick={handleSendMessage}
+                  className={`bg-[#f85671] text-white w-8 h-8 rounded-full flex items-center justify-center flex-shrink-0 transition-all duration-300 ${inputValue ? 'w-8' : 'w-0'
+                    }`}
+                >
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    viewBox='0 0 24 24'
+                    fill='currentColor'
+                    className='size-4 translate-x-[1px] -translate-y-0.5 -rotate-45'
+                  >
+                    <path d='M3.478 2.404a.75.75 0 0 0-.926.941l2.432 7.905H13.5a.75.75 0 0 1 0 1.5H4.984l-2.432 7.905a.75.75 0 0 0 .926.94 60.519 60.519 0 0 0 18.445-8.986.75.75 0 0 0 0-1.218A60.517 60.517 0 0 0 3.478 2.404Z' />
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
