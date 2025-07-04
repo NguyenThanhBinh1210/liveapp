1b60e83362b9f7f9369f04e1bc4fd20e8f880620
NguyenThanhBinh1210 <thanhbinh12369yt@gmail.com>
2025-07-04 18:42:24 +0700

    binh

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,8 @@ import useRouteElements from './routes/useRouteElements'
 const App = () => {
   const routeElements = useRouteElements()
   return (
-    <div>
+    <div className='h-screen'>
       {routeElements}
-
     </div>
   )
 }
